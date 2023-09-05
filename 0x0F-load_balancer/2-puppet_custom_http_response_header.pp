6d4002419b9396a5a0f883d68c3f85765309d85e
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-09-05 07:20:06 -0700

    Automation with puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,52 @@
+# Define Nginx class, installs nginx server and configures it
+
+class { 'nginx':
+	manage_repo => true,
+}
+
+class { 'nginx::package':
+	ensure => 'installed',
+}
+
+file { '/var/www/html/index.html':
+	ensure => 'present',
+	content => "Hello World",
+}
+
+file { '/var/www/html/404.html':
+	ensure => 'present',
+	content => "Ceci n'est pas une page\n",
+}
+
+file { '/etc/nginx/sites-available/default':
+	ensure => 'present',
+	content => "
+server {
+	listen 80;
+	listen [::]:80 default_server;
+	root /var/www/html;
+	index index.html index.htm;
+	add_header X-Served-By $HOSTNAME;
+
+	location /redirect_me {
+		return 301 http://google.com;
+	}
+
+	error_page 404 /404.html;
+	location /404 {
+		root /var/www/html;
+		internal;
+	}
+}
+",
+}
+
+nginx::resource::vhost { 'default':
+	ensure => 'present',
+}
+
+service { 'nginx':
+	ensure 	=> 'running',
+	enable	=> true,
+	suscribe => File['/etc/nginx/sites-available/default'],
+}
