9b0b1be8caaf6a01d8fb534c3bfb119c410ed0e0
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2023-09-25 14:59:19 -0700

    Task 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,9 +2,21 @@
 '''Returns information about an employee TODO list progress'''
 import requests
 import sys
+import json
 
 
 if __name__ == '__main__':
     id = sys.argv[1]
-    response = requests.get('https://jsonplaceholder.typicode.com/todos')
-    print(response)
+    employee = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                            .format(id)).json()
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                         .format(id)).json()
+    name = employee.get('name')
+    total_tasks = len(tasks)
+    completed_tasks = sum(1 for task in tasks if task.get('completed') is True)
+
+    print("Employee {} is done with tasks({}/{})".format(name, completed_tasks,
+                                                         total_tasks))
+    for task in tasks:
+        if task['completed'] is True:
+            print("\t{}".format(task['title']))
