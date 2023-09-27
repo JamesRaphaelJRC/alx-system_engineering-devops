7f587661396d86ee05a14a4b217215f3aca845f4
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-09-27 12:22:40 +0100

    exporting to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+''' Returns information about an employee Todo list progress and stores
+    the output in a csv file
+'''
+import csv
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    id = sys.argv[1]
+    employee = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                            .format(id)).json()
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                         .format(id)).json()
+    name = employee.get('name')
+
+    with open('{}.csv'.format(id), 'w', newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            status = task.get('completed')
+            title = task.get('title')
+            writer.writerow([id, name, status, title])
