de62be6dd949b4f563a3fd2b4b126d548cdba9c2
jamesraphaelhrc <jamesraphaeljrc@gmail.com>
2023-10-10 20:00:00 +0100

    Fixed header

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -23,11 +23,8 @@ import requests
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {"USer-Agent": "MyALXAdvancedAPiProjectBot (by /u/jamesrc)"}
-    
-    params = {
-        "limit": 10
-    }
+    headers = {"User-Agent": "MyALXAdvancedAPiProjectBot (by /u/jamesrc)"}
+    params = {"limit": 10}
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
     if response.status_code == 200:
