170403388375f2ede90be0ff4114fe669f0885a2
jamesraphaelhrc <jamesraphaeljrc@gmail.com>
2023-10-10 17:23:07 +0100

    Used right endpoint

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 def top_ten(subreddit):
     ''' Prints the titles of the first 10 hot posts listed for  given subreddit
     '''
-    url = "https://www.reddit.com/r/{}/comments.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {"USer-Agent": "MyALXAdvancedAPiProjectBot (by /u/jamesrc)"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
@@ -14,6 +14,6 @@ def top_ten(subreddit):
         response = response.json()['data']['children']
         i = 0
         for i in range(10):
-            print(response[i]['data'].get('link_title'))
+            print(response[i]['data'].get('title'))
     else:
         print('None')
