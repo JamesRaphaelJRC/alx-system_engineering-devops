0cb67d2198b48424bea0dcade623ab929693b7aa
jamesraphaelhrc <jamesraphaeljrc@gmail.com>
2023-10-10 17:12:57 +0100

    Task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' Querries the Reddit API '''
+import requests
+
+
+def top_ten(subreddit):
+    ''' Prints the titles of the first 10 hot posts listed for  given subreddit
+    '''
+    url = "https://www.reddit.com/r/{}/comments.json".format(subreddit)
+    headers = {"USer-Agent": "MyALXAdvancedAPiProjectBot (by /u/jamesrc)"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        response = response.json()['data']['children']
+        i = 0
+        for i in range(10):
+            print(response[i]['data'].get('link_title'))
+    else:
+        print('None')
